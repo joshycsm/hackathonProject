3b32a87793b742940352f8809215c884b33079f8
github username <github email address>
2020-02-04 12:56:41 -0700

    relationships, models, and tables created but seed file not working; can't run rails db:seed

---
+++ astronomicalBackend/db/migrate/20200204192342_create_images.rb
@@ -0,0 +1,12 @@
+class CreateImages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :images do |t|
+      t.string :date
+      t.text :explanation
+      t.string :title
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end

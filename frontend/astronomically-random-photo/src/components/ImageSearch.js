ede664faf41b1fc4d5caf1e3d6451fb1082d74d9
hoidal <sjhoidal@gmail.com>
2020-02-04 15:34:21 -0700

    finished styling

--- frontend/astronomically-random-photo/src/components/ImageSearch.js
+++ frontend/astronomically-random-photo/src/components/ImageSearch.js
@@ -2,17 +2,17 @@ import React from "react"
 
 const ImageSearch = (props) => {
 
-    // const updateSearchTerm = event => {
-    //     props.updateSearchTerm(event.target.value)
-    // }
+    const updateSearchTerm = event => {
+        props.updateSearchTerm(event.target.value)
+    }
 
     return (
         <form className="image-filter">
             <input
                 type="text"
-                // value={props.searchTerm}
+                value={props.searchTerm}
                 placeholder="Search Images"
-                // onChange={updateSearchTerm}
+                onChange={updateSearchTerm}
             />
         </form>
     )
